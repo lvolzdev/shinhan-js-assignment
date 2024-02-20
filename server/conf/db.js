2a11eab28a961a704aed4972d3e24f86eeb68ee4
se0983 <se098300@gmail.com>
2024-02-21 02:14:31 +0900

    modify: db connection file update

---
+++ server/conf/db.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const confDB = {
+    username: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    cluster: process.env.CLUSTER,
+    database: process.env.DATEBASE,
+    getMongoURL: function () {
+        return `mongodb+srv://${this.username}:${this.password}@${this.cluster}/${this.database}`;
+    },
+};
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            confDB.getMongoURL(), {
+                retryWrites: true,
+                w: 'majority',
+            }
+        );
+        console.log('DB Connection Successful!');
+    } catch(err) {
+        console.error('DB Connection Failed:', err);
+    }
+}
+
+const disconnectDB = async () => {
+    try {
+        await mongoose.disconnect();
+        console.log('DB Disconnected Successful!');
+    } catch(err) {
+        console.error('DB Disconnection Failed:', err);
+    }
+}
+
+module.exports = { 
+    confDB,
+    connectDB, 
+    disconnectDB 
+};
