6e6672a70ae2b50b3d383305a7e64a97c8ac6eb5
Sieun Kim <se0983@kookmin.ac.kr>
2024-02-22 00:05:43 +0900

    feat: start to react project

---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:5003",
+    },
+  },
+  resolve: {
+    alias: [
+      // 절대경로로 접근하기
+      { find: "~/components", replacement: "/src/components" },
+      { find: "~/lib", replacement: "/src/lib" },
+      { find: "~/routers", replacement: "/src/routers" },
+      { find: "~/routes", replacement: "/src/routes" },
+      { find: "~/store", replacement: "/src/store" }, // 전역으로 접근하기 위해 추가
+    ],
+  },
+});
