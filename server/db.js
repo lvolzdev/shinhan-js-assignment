e0a13168a788cdd8e21ddd0ad4fa510f37d1c07f
Sieun Kim <se0983@kookmin.ac.kr>
2024-02-21 17:27:52 +0900

    Chore: Other settings

---
+++ server/db.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const confDB = {
+  username: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  cluster: process.env.CLUSTER,
+  database: process.env.DATEBASE,
+  getMongoURL: function () {
+    return `mongodb+srv://${this.username}:${this.password}@${this.cluster}/${this.database}`;
+  },
+};
+
+const connectDB = async () => {
+  try {
+    console.log("여기야");
+    console.log(process.env.USER_NAME);
+    // console.log(process.env);
+    // console.log(confDB.getMongoURL());
+    await mongoose.connect(confDB.getMongoURL(), {
+      retryWrites: true,
+      w: "majority",
+    });
+    console.log("DB Connection Successful!");
+  } catch (err) {
+    console.error("DB Connection Failed:", err);
+  }
+};
+
+const disconnectDB = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log("DB Disconnected Successful!");
+  } catch (err) {
+    console.error("DB Disconnection Failed:", err);
+  }
+};
+
+module.exports = {
+  confDB,
+  connectDB,
+  disconnectDB,
+};
--- server/models/Campaign.js
+++ server/models/Campaign.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
-const { connectDB, disconnectDB } = require('../conf/db');
-const Comment = require('./Comment');
+const mongoose = require("mongoose");
+const { connectDB, disconnectDB } = require("../db");
+const fs = require("fs");
+const Comment = require("./Comment");
 
-const campaignSchema = new mongoose.Schema({
-    campaignId: { type: Number, required: true },
+const campaignSchema = new mongoose.Schema(
+  {
+    campaignId: { type: String, required: true },
     categoryName: { type: String, required: true },
     title: { type: String, required: true },
     totalBackedAmount: { type: Number, required: true }, // 총모집금액(인권)
@@ -12,10 +14,37 @@ const campaignSchema = new mongoose.Schema({
     coreMessage: { type: String },
     whenOpen: { type: Date, required: true }, // 오픈일자
     achievementRate: { type: Number, required: true }, // 달성률
-}, {
-    timestamps: true
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Campaign = mongoose.model('Campaign', campaignSchema);
+const Campaign = mongoose.model("Campaign", campaignSchema);
+
+// DB에 데이터 저장
+async function saveDataToDB() {
+  try {
+    await connectDB();
+
+    const jsonData = fs.readFileSync(
+      "../crawler/res/campaignList.json",
+      "utf-8"
+    );
+    const campaigns = JSON.parse(jsonData);
+
+    await Campaign.insertMany(campaigns);
+    console.log("Data save to db successfully!");
+  } catch (err) {
+    console.error("Error saving data to db:", err);
+  } finally {
+    await disconnectDB();
+  }
+}
 
 module.exports = Campaign;
+
+// 현재 파일이 직접 실행될 때만 아래 함수 실행되도록
+if (require.main === module) {
+  saveDataToDB();
+}
