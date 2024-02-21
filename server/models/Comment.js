5498be20c929f4ed832c5cb7b9d7750b599d3080
Sieun Kim <se0983@kookmin.ac.kr>
2024-02-21 23:54:18 +0900

    chore: Comment 스키마 수정

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -12,13 +12,12 @@ const commentSchema = new mongoose.Schema({
     required: true,
   },
   commentType: { type: String }, // 없는 경우 있음
-  userNickName: { type: String }, // 없는 경우 있음
+  userNickName: { type: String }, // 없는 경우 있음 // TODO
   whenCreated: { type: Date, default: Date.now },
   commentId: {
     // type: mongoose.Schema.Types.ObjectId,
     type: String,
     ref: "Comment",
-    required: true,
     default: null, // 댓글인 경우 null, 대댓글인 경우 참조 댓글의 _id
   },
   depth: { type: Number, required: true, default: 0 },
