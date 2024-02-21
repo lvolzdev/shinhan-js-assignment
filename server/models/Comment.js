df1680494a2dbde7cb2cfc6484c8b05c304acb3a
Sieun Kim <se0983@kookmin.ac.kr>
2024-02-21 17:27:17 +0900

    modify: Comment model schema

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,15 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-    body: { type: String, required: true },
-    campaign: { type: mongoose.Schema.Types.ObjectId, ref: 'Campaign', required: true },
-    commentType: { type: String },
-    userNickname: { type: String, required: true },
-    whenCreated: { type: Date, default: Date.now() },
-    // commentReplys: {},
-    depth: { type: Number, required: true, default: 0 },
+  body: { type: String, required: true },
+  campaignId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Campaign",
+    required: true,
+  },
+  commentType: { type: String },
+  userNickname: { type: String, required: true },
+  whenCreated: { type: Date, default: Date.now },
+  commentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment",
+    required: true,
+    default: null, // 댓글인 경우 null, 대댓글인 경우 참조 댓글의 _id
+  },
+  depth: { type: Number, required: true, default: 0 },
 });
 
-const Comment = mongoose.model('Comment', commentSchema);
+const Comment = mongoose.model("Comment", commentSchema);
 
 module.exports = Comment;
