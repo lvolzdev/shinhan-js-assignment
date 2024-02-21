1053f8119250fe4ae3f9b6a87a8d170c1ed19bfe
Sieun Kim <se0983@kookmin.ac.kr>
2024-02-21 19:50:40 +0900

    feat: 대댓글 comment reply 파싱

--- server/crawler/campaignList.js
+++ server/crawler/campaignList.js
@@ -1,58 +1,59 @@
-import axios from 'axios';
+import axios from "axios";
 import fs from "fs";
 
-const baseURL = 'https://service.wadiz.kr/api/search/funding';
+const baseURL = "https://service.wadiz.kr/api/search/funding";
 
 const payload = {
-    "startNum": 0,
-    "order": "support", // 응원참여자순 정렬
-    "limit": 20,
-    "categoryCode": "",
-    "endYn": ""
+  startNum: 0,
+  order: "support", // 응원참여자순 정렬
+  limit: 20,
+  categoryCode: "",
+  endYn: "",
 };
 const headers = {
-    'Referer': 'https://www.wadiz.kr/',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-    'Content-Type': 'application/json'
+  Referer: "https://www.wadiz.kr/",
+  "User-Agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+  "Content-Type": "application/json",
 };
 
 async function wadizFetch() {
-    try {
-        const resp = await axios.post(baseURL, payload, headers);
-        const $ = resp.data.data.list;
-        // const result = JSON.parse(resp.data);
-        // console.log($);
+  try {
+    const resp = await axios.post(baseURL, payload, headers);
+    const $ = resp.data.data.list;
+    // const result = JSON.parse(resp.data);
+    // console.log($);
 
-        const result = $.map((el, i) => {
-            return {
-                campaignId: el.campaignId,
-                categoryName: el.categoryName,
-                title: el.title,
-                totalBackedAmount: el.totalBackedAmount,
-                photoUrl: el.photoUrl,
-                nickName: el.nickName,
-                coreMessage: el.coreMessage,
-                whenOpen: new Date(el.whenOpen),
-                achievementRate: el.achievementRate
-            }
-        });
-        console.log(result)
-        const jsonString = JSON.stringify(result);
-        fs.writeFileSync("./res/campaignList.json", jsonString);
-        console.log('Successful saving of campaign json data!');
-    } catch (err) {
-        console.log(err);
-        console.log('Failed to save campaign json data');
-        return null;
-    }
-};
+    const result = $.map((el, i) => {
+      return {
+        campaignId: el.campaignId,
+        categoryName: el.categoryName,
+        title: el.title,
+        totalBackedAmount: el.totalBackedAmount,
+        photoUrl: el.photoUrl,
+        nickName: el.nickName,
+        coreMessage: el.coreMessage,
+        whenOpen: new Date(el.whenOpen),
+        achievementRate: el.achievementRate,
+      };
+    });
+    console.log(result);
+    const jsonString = JSON.stringify(result);
+    fs.writeFileSync("./res/campaignList.json", jsonString);
+    console.log("Successful saving of campaign json data!");
+  } catch (err) {
+    console.log(err);
+    console.log("Failed to save campaign json data");
+    return null;
+  }
+}
 
 async function main() {
-    try {
-        wadizFetch();
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await wadizFetch();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 main();
--- server/crawler/comment.js
+++ server/crawler/comment.js
@@ -1,72 +1,90 @@
-import axios from 'axios';
+import axios from "axios";
 import fs from "fs";
 
-const baseURL = 'https://www.wadiz.kr/web/reward/api/comments/campaigns';
+const baseURL = "https://www.wadiz.kr/web/reward/api/comments/campaigns";
 
 const payload = {
-    "page": 0,
-    "size": 20,
-    "commentGroupType": "CAMPAIGN",
-    "rewardCommentType": null,
+  page: 0,
+  size: 20,
+  commentGroupType: "CAMPAIGN",
+  rewardCommentType: null,
 };
 
 async function getCampaignIds() {
-    try {
-        const jsonData = fs.readFileSync('./res/campaignListOutput.json', 'utf-8');
-        const campaignId = JSON.parse(jsonData);
+  try {
+    const jsonData = fs.readFileSync("./res/campaignList.json", "utf-8");
+    const campaignId = JSON.parse(jsonData);
 
-        const campaignIds = campaignId.map(camp => camp.campaignId);
-        return campaignIds;
-    } catch(err) {
-        console.log(err);
-        return [];
-    }
-};
+    const campaignIds = campaignId.map((camp) => camp.campaignId);
+    return campaignIds;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
 
 async function wadizCommentFetch() {
-    try {
-        // campaign Id 가져오기
-        const campaignIds = await getCampaignIds();
-        let allResults = [];
+  try {
+    const campaignIds = await getCampaignIds();
+    let allResults = [];
 
-        for (const campaignId of campaignIds) {
-            const URL = `${baseURL}/${campaignId}`;
-            const resp = await axios.get(URL, {
-                params: payload
-            });
-            const $ = resp.data.data.content;
-            // console.log($);
+    for (const campaignId of campaignIds) {
+      const URL = `${baseURL}/${campaignId}`;
+      const resp = await axios.get(URL, {
+        params: payload,
+      });
+      const comments = resp.data.data.content;
 
-            const result = $.map((el, i) => {
-                return {
-                    body: el.body,
-                    campaign: el.commontId, // TODO
-                    commentType: el.commentType,
-                    userNickName: el.nickName,
-                    whenCreated: el.whenCreated,
-                    commentReplys: el.commentReplys, // 대댓글
-                    depth: el.depth, // 대댓글 깊이
-                }
-            });
-            allResults = allResults.concat(result);
-            console.log(`Parsing: ${campaignId}`);
+      let results = [];
+      for (const comment of comments) {
+        const data = {
+          body: comment.body,
+          campaignId: comment.commonId,
+          commentType: comment.commentType,
+          userNickName: comment.nickName,
+          whenCreated: comment.whenCreated,
+          commentId: comment.boardId,
+          hasReply: comment.hasReply,
+          //   commentReplys: comment.commentReplys,
+          depth: comment.depth, // 0
+        };
+        results.push(data);
+
+        // 대댓글 -> url이 댓글과 같아서 또 요청할 필요가 없음
+        if (comment.hasReply) {
+          for (const reply of comment.commentReplys) {
+            const replyData = {
+              body: reply.body,
+              campaignId: reply.commonId,
+              commentType: reply.commentType, // TODO
+              userNickName: reply.nickName,
+              whenCreated: reply.whenCreated,
+              commentId: reply.parentBoardId, // 상위 댓글의 id
+              depth: reply.depth, // 1
+            };
+            results.push(replyData);
+          }
         }
-        const jsonString = JSON.stringify(allResults);
-        fs.writeFileSync('./res/comment.json', jsonString);
-        console.log('Successful saving of comment json data!');
-    } catch (err) {
-        console.log('Failed to save comment json data');
-        console.log(err);
-        return null;
+      }
+      allResults = allResults.concat(results);
+      console.log(`Parsing: ${campaignId}`);
     }
-};
+
+    const jsonString = JSON.stringify(allResults);
+    fs.writeFileSync("./res/comment.json", jsonString);
+    console.log("Successful saving of comment json data!");
+  } catch (err) {
+    console.log("Failed to save comment json data");
+    console.log(err);
+  }
+}
 
 async function main() {
-    try {
-        wadizCommentFetch();
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await wadizCommentFetch();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 main();
